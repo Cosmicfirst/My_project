494483681ee05a90083a4e17051341903c4ca57f
Cosmicfirst <studentadventure@gmail.com>
2024-08-16 14:22:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// Homework 21
+// Varianta 1
+// Stocam masa si greutatea eroilor nostri
+// const massVadim = 78;
+// const heightVadim = 1.69;
+// const massAlex = 92;
+// const heightAlex = 1.95;
+
+// let BMIVadim = massVadim / heightVadim ** 2;
+// let BMIAlex = massAlex / heightAlex ** 2;
+
+// // creare variabila tip boolean
+// let markHigherMass = BMIVadim > BMIAlex;
+// console.log(markHigherMass);
+// console.log("Vadim are un indice de masa mai mare decat Alex");
+
+// Varianta 2
+// Stocam masa si greutatea eroilor nostri
+const massVadim = 95;
+const heightVadim = 1.88;
+const massAlex = 85;
+const heightAlex = 1.76;
+
+let BMIVadim = massVadim / heightVadim ** 2;
+let BMIAlex = massAlex / heightAlex ** 2;
+
+// creare variabila tip boolean
+let markHigherMass = BMIVadim > BMIAlex;
+console.log(markHigherMass);
+console.log("Vadim are un indice de masa mai mic decat Alex");
